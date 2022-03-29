c0e5c741dd2c159da2049bd6c20a9177a3c69a62
starkunpyf <2560331078@qq.com>
2022-03-29 16:14:06 +0800

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App欢迎"/>
   </div>
 </template>
 
